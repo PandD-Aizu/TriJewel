39bc363416c497e54e29efc028627835824b0b95
KENnY <s1250124@u-aizu.ac.jp>
2021-09-12 15:06:57 +0900

    ストーリー再生画面、サンプルテキストの再生を実装

--- TriJewel/story.cpp
+++ TriJewel/story.cpp
@@ -2,28 +2,43 @@
 
 static Actor actors[6];	// キャラクター達
 static Array<Dialogue> dialogue;	// セリフ
+static int scene;	// セリフ番号
+
+static int start;
 
 // ストーリーの初期化
 void story_init(int chapter, int story) {
 	dialogue = {
-		Dialogue(0,1,U"今日はいい天気ですね。",1),
+		Dialogue(0,1,U"今日はいい天気ですね。\n気分がいいです。",1),
 		Dialogue(0,1,U"はい。私もそう思います。", 2),
 		Dialogue(-1,-1,U"しばらくして...",0),
 		Dialogue(1,2,U"月が綺麗ですね。",1),
 		Dialogue(1,2,U"その通りです。",2)
 	};
+
+	scene = 0;
+	start = Scene::Time() * 1000;
 }
 
 // ストーリーの更新関数
 // 戻り値: ストーリー再生中の場合は 0
 // 　　　  再生を終えた場合は 1 を返す
 int story_update() {
-	/*** ここを編集してください ***/
+	if (MouseL.down()) {
+		if (scene == dialogue.size() - 1)
+			return 1;
+		
+		scene++;
+		start = Scene::Time() * 1000;
+	}
 
 	return 0;
 }
 
 // ストーリーの描画関数
 void story_draw() {
-	/*** ここを編集してください ***/
+	const int length = (int)((Scene::Time() * 1000 - start) / 50);
+
+	Rect textbox = Rect(Scene::Width() / 16, (Scene::Height() / 16) * 12, (Scene::Width() / 16) * 14, (Scene::Height() / 16) * 3).draw(Palette::White);
+	FontAsset(U"StoryFont")(dialogue[scene].text.substr(0, length)).draw(textbox.stretched(-10), Palette::Black);
 }
