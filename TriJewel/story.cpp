02c7c688b4bac1ff0c73cf49d14394f92316f5bb
KENnY <s1250124@u-aizu.ac.jp>
2021-09-14 17:13:17 +0900

    Story

--- TriJewel/story.cpp
+++ TriJewel/story.cpp
@@ -4,20 +4,51 @@ static Actor actors[6];	// キャラクター達
 static Array<Dialogue> dialogue;	// セリフ
 static int scene;	// セリフ番号
 
-static int start;
+static int playing;	// セリフ再生中かどうかフラグ
+static int start;	// 開始時間(セリフ再生のために必要)
+static int length;	// 表示させるセリフの長さ
 
 // ストーリーの初期化
 void story_init(int chapter, int story) {
-	dialogue = {
-		Dialogue(0,1,U"今日はいい天気ですね。\n気分がいいです。",1),
-		Dialogue(0,1,U"はい。私もそう思います。", 2),
-		Dialogue(-1,-1,U"しばらくして...",0),
-		Dialogue(1,2,U"月が綺麗ですね。",1),
-		Dialogue(1,2,U"その通りです。",2)
-	};
+	// ファイル読み込み用変数
+	TextReader reader(U"Data/Story/test.txt");
+	String line;
+
+	// 読み込みデータ
+	int left, right;
+	String text;
+	int speaker;
+
+	// ファイルが読み込めなかった場合、エラーメッセージを表示させる
+	if (!reader){
+		throw Error(U"Failed to open `test.txt`");
+	}
+
+	dialogue.clear();
+
+	// ファイル読み込み
+	while (1) {
+		reader.readLine(line);
+		left = Parse<int>(line);
+
+		reader.readLine(line);
+		right = Parse<int>(line);
+		
+		reader.readLine(text);
+		
+		reader.readLine(line);
+		speaker = Parse<int>(line);
+
+		dialogue.push_back(Dialogue(left, right, text, speaker));
+
+		if (!reader.readLine(line)) break;
+	}
+
+	reader.close();
 
 	scene = 0;
 	start = Scene::Time() * 1000;
+	playing = 0;
 }
 
 // ストーリーの更新関数
@@ -25,11 +56,23 @@ void story_init(int chapter, int story) {
 // 　　　  再生を終えた場合は 1 を返す
 int story_update() {
 	if (MouseL.down()) {
-		if (scene == dialogue.size() - 1)
-			return 1;
-		
-		scene++;
-		start = Scene::Time() * 1000;
+		if (playing == 1) {
+			if (scene == dialogue.size() - 1)
+				return 1;
+
+			scene++;
+			start = Scene::Time() * 1000;
+			playing = 0;
+		}
+		else {
+			length = dialogue[scene].text.length();
+		}
+	}
+
+	length = (int)((Scene::Time() * 1000 - start) / 50);
+
+	if (length > dialogue[scene].text.length()) {
+		playing = 1;
 	}
 
 	return 0;
@@ -37,16 +80,25 @@ int story_update() {
 
 // ストーリーの描画関数
 void story_draw() {
-	const int length = (int)((Scene::Time() * 1000 - start) / 50);
 	const int namebox_size = 65;
 	const int chara_size = 128;
+	const String name[6] = {
+		U"シロナ",
+		U"リンドル",
+		U"チャマ",
+		U"ファイ",
+		U"エメ",
+		U"メルヴィ"
+	};
 
-	Rect(Scene::Width() / 16, (Scene::Height() / 16) * 12 - chara_size - namebox_size - 10, chara_size, chara_size).draw(Palette::Yellow);
-	Rect(Scene::Width() / 16 * 15 - chara_size, (Scene::Height() / 16) * 12 - chara_size - namebox_size - 10, chara_size, chara_size).draw(Palette::Yellow);
+	if (dialogue[scene].left >= 0)
+		TextureAsset(name[dialogue[scene].left]).drawAt(Scene::Width() / 16 + chara_size / 2, (Scene::Height() / 16) * 12 - chara_size - namebox_size - 10 + chara_size / 2);
+	if (dialogue[scene].right >= 0)
+		TextureAsset(name[dialogue[scene].right]).drawAt(Scene::Width() / 16 * 15 - chara_size + chara_size / 2, (Scene::Height() / 16) * 12 - chara_size - namebox_size - 10 + chara_size / 2);
 
 	if (dialogue[scene].speaker != 0) {
 		Rect namebox = Rect(Scene::Width() / 16, (Scene::Height() / 16) * 12 - namebox_size, 150, namebox_size).draw(Palette::White).drawFrame(1, 0, Palette::Black);
-		FontAsset(U"StoryFont")(U"キャラ{}"_fmt(dialogue[scene].speaker)).draw(namebox.stretched(-10), Palette::Black);
+		FontAsset(U"StoryFont")(name[dialogue[scene].speaker == 1 ? dialogue[scene].left : dialogue[scene].right]).draw(namebox.stretched(-10), Palette::Black);
 	}
 
 	Rect textbox = Rect(Scene::Width() / 16, (Scene::Height() / 16) * 12, (Scene::Width() / 16) * 14, (Scene::Height() / 16) * 3).draw(Palette::White).drawFrame(1, 0, Palette::Black);
