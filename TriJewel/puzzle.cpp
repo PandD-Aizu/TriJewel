797cb1fd23944d756f8390a19bfe6e6a758c9e75
Taityan <s1290202@u-aizu.ac.jp>
2021-09-14 16:03:01 +0900

    aaaa

--- TriJewel/puzzle.cpp
+++ TriJewel/puzzle.cpp
@@ -8,98 +8,148 @@ static Player player;
 void puzzle_init(int diff, int stage) {
 
 	// ステージ読み込み
+
 	stage_data = {
-		{1,1,0,0,3},
-		{1,1,0,0,0},
-		{0,0,2,0,0},
-		{0,0,0,1,1},
-		{0,0,0,1,1}
+		{4,4,4,4,4,4,4},
+		{4,1,1,0,0,3,4},
+		{4,1,1,0,0,0,4},
+		{4,0,0,2,0,0,4},
+		{4,0,0,0,1,1,4},
+		{4,0,0,0,1,1,4},
+		{4,4,4,4,4,4,4}
 	};
 
 	// プレイヤー初期座標
-	player.i = 230;
-	player.j = 350;
+	player.i = 5;
+	player.j = 1;
 
-	int d[5][5];
+	player.x = 130;
+	player.y = 250;
 
-	for (int i = 0; i < wallx; i++) {
-		for (int j = 0; j < wally; j++) {
-			d[i][j] = 0;
-		}
-	}
+	if (player.x < 100) player.x = 100;
+	if (player.x > 250) player.x = 250;
+	if (player.y < 100) player.y = 100;
+	if (player.y > 250) player.y = 250;
+
+	if (player.i < 0) player.i = 0;
+	if (player.i > stage_data.height()) player.i = stage_data.height();
+	if (player.j < 0) player.j = 0;
+	if (player.j > stage_data.width()) player.j = stage_data.width();
+
+
 }
 
 // パズルの更新関数
 // 戻り値: パズルを続行中の場合は 0
 // 　　　  クリアした場合は 1 を返す
 int puzzle_update() {
-	int a = 0, b = 4;
 
-	if (KeyUp.down() && d[a][b-1]==0) player.j -= 30,b--;
-	if (KeyDown.down() && d[a][b+1] == 0) player.j += 30,b++;
-	if (KeyRight.down() && d[a+1][b] == 0) player.i += 30,a++;
-	if (KeyLeft.down() && d[a-1][b] == 0) player.i -= 30,a--;
+	if (KeyUp.down()) {
+		switch (stage_data[player.i - 1][player.j]) {
+		case 0:
+			player.y -= 30, player.i--;
+			break;
+		case 2:
+			if (stage_data[player.i - 2][player.j] == 0) {
+				stage_data[player.i - 1][player.j] = 0, stage_data[player.i - 2][player.j] = 2, player.y -= 30, player.i--;
+			}
+			break;
+		case 3:
+			player.y -= 30, player.i--;
+			return 1;
+			break;
+		}
+	}
+	else if (KeyDown.down()) {
+		switch (stage_data[player.i + 1][player.j]) {
+		case 0:
+			player.y += 30, player.i++;
+			break;
+		case 2:
+			if (stage_data[player.i + 2][player.j] == 0) {
+				stage_data[player.i + 1][player.j] = 0, stage_data[player.i + 2][player.j] = 2, player.y += 30, player.i++;
+			}
+			break;
+		case 3:
+			player.y += 30, player.i++;
+			return 1;
+			break;
+		}
+	}
+	else if (KeyRight.down()) {
+		switch (stage_data[player.i][player.j + 1]) {
+		case 0:
+			player.x += 30, player.j++;
+			break;
+		case 2:
+			if (stage_data[player.i][player.j + 2] == 0) {
+				stage_data[player.i][player.j + 1] = 0, stage_data[player.i][player.j + 2] = 2, player.x += 30, player.j++;
+			}
+			break;
+		case 3:
+			player.x += 30, player.j++;
+			return 1;
+			break;
+		}
+	}
+	else if (KeyLeft.down()) {
+		switch (stage_data[player.i][player.j - 1]) {
+		case 0:
+			player.x -= 30, player.j--;
+			break;
+		case 2:
+			if (stage_data[player.i][player.j - 2] == 0) {
+				stage_data[player.i][player.j - 1] = 0, stage_data[player.i][player.j - 2] = 2, player.x -= 30, player.j--;
+			}
+			break;
+		case 3:
+			player.x -= 30, player.j--;
+			return 1;
+			break;
+		}
+	}
 	/*** ここを編集してください ***/
 	return 0;
 }
 
 // パズルの描画関数
 void puzzle_draw() {
-	int wallx = 6, wally = 6, goalx = 350, goaly = 350, Startx = 200, Starty = 200, Blockx = 290, Blocky = 290;
-	int limitx = 0, limity = 0;
-	int d[5][5];
-
-	//壁
-	for (int j = 0; j < wallx; j++) {
-		Rect(Startx + j * 30, Starty, 30, 30).draw(Palette::Gray);
-	}
-
-	for (int i = 0; i < wally; i++) {
-		Rect(Startx, Starty + i * 30, 30, 30).draw(Palette::Gray);
-	}
-
-	for (int k = 0; k <= wally; k++) {
-		Rect(Startx + 30 * wallx, Starty + k * 30, 30, 30).draw(Palette::Gray);
-	}
-
-	for (int l = 0; l <= wallx; l++) {
-		Rect(Startx + l * 30, Starty + wally * 30, 30, 30).draw(Palette::Gray);
-	}
-
-	//地面
-	for (int i = 0; i < wallx - 1; i++) {
-		for (int j = 0; j < wally - 1; j++) {
-			Rect((Startx + 30) + i * 30, (Starty + 30) + j * 30, 30, 30).draw(Palette::Green);
-		}
-	}
-
-	//壁追加
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			Rect(Startx + (i + 1) * 30, Starty + (j + 1) * 30, 30, 30).draw(Palette::Gray);
-			d[i][j] = 1;
-		}
-	}
 
 
 
-	for (int i = 3; i < 5; i++) {
-		for (int j = 3; j < 5; j++) {
-			Rect(Startx + (i + 1) * 30, Starty + (j + 1) * 30, 30, 30).draw(Palette::Gray);
-			d[i][j] = 1;
-		}
-	}
-
-
-
-	Rect(Blockx, Blocky, 30, 30).draw(Palette::Black);
-
-
-	//ゴール
-	Rect(goalx, goaly, 30, 30).draw(Palette::Yellow);
-	//主人公
-	Rect(player.i, player.j, 30, 30).draw(Palette::Blue);
 	/*** ここを編集してください ***/
+
+	for (int i = 0; i < stage_data.width(); i++) {
+		for (int j = 0; j < stage_data.height(); j++) {
+			switch (stage_data[i][j]) {
+			case 0:
+				Rect(100 + j * 30, 100 + i * 30, 30, 30).draw(Palette::Green);
+				break;
+
+			case 1:
+				Rect(100 + j * 30, 100 + i * 30, 30, 30).draw(Palette::Gray);
+				break;
+
+			case 2:
+				Rect(100 + j * 30, 100 + i * 30, 30, 30).draw(Palette::Orange);
+				break;
+
+			case 3:
+				Rect(100 + j * 30, 100 + i * 30, 30, 30).draw(Palette::Yellow);
+				break;
+
+
+			case 4:
+				Rect(100 + j * 30, 100 + i * 30, 30, 30).draw(Palette::Black);
+				break;
+
+
+
+			}
+		}
+	}
+	//主人公
+	Rect(player.x, player.y, 30, 30).draw(Palette::Blue);
 }
 
 bool objmove() {
