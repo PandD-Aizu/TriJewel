c262e6faf4942760955e7248cef174d687f61718
KENnY <s1250124@u-aizu.ac.jp>
2021-10-10 15:50:03 +0900

    スタッフクレジットを作成

--- TriJewel/Main.cpp
+++ TriJewel/Main.cpp
@@ -290,7 +290,8 @@ public:
             break;
         }
 
-        FontAsset(U"TitleFont")(U"ステージセレクト").draw(180, 100, Palette::Gray);
+        FontAsset(U"TitleFont")(U"ステージセレクト").draw(184, 104, Palette::Gray);
+        FontAsset(U"TitleFont")(U"ステージセレクト").draw(180, 100, Palette::Lightgreen);
     }
 };
 
@@ -439,6 +440,16 @@ public:
                 }
             }
 
+            // スタッフクレジット
+            if (SimpleGUI::Button(U"スタッフクレジット", Vec2(500, Scene::Height() / 4 * 3))) {
+                AudioAsset(U"se_click").playOneShot();
+
+                chapter = 0;
+                chapter_before = chapter;
+                story = 0;
+                changeScene(U"Staff");
+            }
+
             break;
 
         case 1:
@@ -489,6 +500,9 @@ public:
             for (int i = 0; i < CHAPTER_NUM; i++) {
                 SimpleGUI::Button(chap_str[i], Vec2(100 + 200 * i, Scene::Height() / 2 - 25), 180);
             }
+
+            SimpleGUI::Button(U"スタッフクレジット", Vec2(500, Scene::Height() / 4 * 3));
+
             break;
 
             // 話数選択
@@ -506,7 +520,8 @@ public:
             break;
         }
 
-        FontAsset(U"TitleFont")(U"ストーリーセレクト").drawAt(400, 100);
+        FontAsset(U"TitleFont")(U"ストーリーセレクト").drawAt(404, 104, Palette::Gray);
+        FontAsset(U"TitleFont")(U"ストーリーセレクト").drawAt(400, 100, Palette::White);
     }
 };
 
@@ -593,7 +608,87 @@ public:
 
         SimpleGUI::Button(U"タイトル", Vec2(10, 10));
 
+        FontAsset(U"TitleFont")(U"あそびかた").drawAt(404, 104, Palette::Gray);
         FontAsset(U"TitleFont")(U"あそびかた").drawAt(400, 100);
+    }
+};
+
+// スタッフクレジットシーン
+class Staff : public App::Scene
+{
+private:
+    Array<String> credit;   // スタッフクレジット(一行ずつ格納する)
+    int scroll;   // スクロール位置(y座標)
+
+public:
+
+    // コンストラクタ（必ず実装）
+    Staff(const InitData& init)
+        : IScene(init)
+    {
+        scroll = Scene::Height() / 2 + 200;
+
+        TextReader reader;
+        String line;
+
+        if (!reader.open(U"Data/staff.txt")) {
+            throw Error(U"Failed to open staff.txt");
+        }
+
+        // スタッフクレジット情報の読み込み
+        while (reader.readLine(line)) {
+            // 空行は大きなスペース
+            if (line.isEmpty()) {
+                for (int i = 0; i < 3; i++) {
+                    credit << U"";
+                }
+            }
+
+            // 一行ずつ読み込む
+            else {
+                credit << line;
+            }
+        }
+
+        reader.close();
+
+        AudioAsset(U"bgm_ed").play();
+    }
+
+    // 更新関数
+    void update() override
+    {
+        scroll -= 1;
+
+        // ストーリー選択画面へ戻る
+        if (MouseR.down() || !AudioAsset(U"bgm_ed").isPlaying())
+        {
+            AudioAsset(U"bgm_ed").stop();
+            changeScene(U"StorySelect");
+        }
+    }
+
+    // 描画関数 (const 修飾)
+    void draw() const override
+    {
+        String fontName;
+        Color color;
+
+        TextureAsset(U"logo").drawAt(Scene::Width() / 2, scroll - 200);
+
+        for (int i = 0; i < credit.size(); i++) {
+            if (credit[i][0] == '(')
+                fontName = U"StoryFont";
+            else
+                fontName = U"TitleFont";
+
+            if (credit[i][0] == '<')
+                color = Palette::Lightgreen;
+            else
+                color = Palette::White;
+
+            FontAsset(fontName)(credit[i]).drawAt(Scene::Width() / 2, scroll + 60 * i, color);
+        }
     }
 };
 
@@ -646,6 +741,7 @@ void Main()
     AudioAsset::Register(U"bgm_clear", U"Data/Sound/bgm/clear.ogg");
     AudioAsset::Register(U"bgm_story1", U"Data/Sound/bgm/story.ogg");
     AudioAsset::Register(U"bgm_story2", U"Data/Sound/bgm/story2.ogg");
+    AudioAsset::Register(U"bgm_ed", U"Data/Sound/bgm/ed_theme.wav");
 
 	// シーンマネージャーを作成
 	App manager;
@@ -657,6 +753,7 @@ void Main()
     manager.add<StorySelect>(U"StorySelect");
     manager.add<Story>(U"Story");
     manager.add<HowToPlay>(U"HowToPlay");
+    manager.add<Staff>(U"Staff");
 
     readSave();
     readCaption();
